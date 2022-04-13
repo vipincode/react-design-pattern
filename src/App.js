6a258ff0d0ea158a05b1e07bcd951e054d4ef3e9
Vipin Singh <svipin174@gmail.com>
2022-04-13 08:30:02 +0530

    fix: splict layout creating via passing children as props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { SplitScreen } from './SplitScreen';
 
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+const LeftHandComponent = ({name}) => {
+  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
 };
 
-const RighttHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'red' }}>Right!</h1>;
+const RighttHandComponent = ({message}) => {
+  return <h1 style={{ backgroundColor: 'red' }}>{message}</h1>;
 };
 
 function App() {
   return (
     <div className="App">
+      {/* passing weight props here, 1/3 form */}
       <SplitScreen leftWeight={1} rightWeight={3}>
-        <LeftHandComponent />
-        <RighttHandComponent />
+        <LeftHandComponent name="Vipin Singh" />
+        <RighttHandComponent message="Hello!" />
       </SplitScreen>
     </div>
   );
--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -5,13 +5,17 @@ const Container = styled.div`
 `;
 
 const Pane = styled.div`
-  flex: ${props => props.weight};
+  /* these value comes from pane props */
+  flex: ${props => props.weight}; 
 `;
 
+// destructuring property `leftWeight=1, rightWeight=1` thse are defalut value for `weight, 1`
 export function SplitScreen({ children, leftWeight=1, rightWeight=1 }) {
+  // here children accep multiple component, so declare t array, see in App.js how to pass children
   const [left, right] = children
   return (
     <Container>
+      {/* access props props in styled css */}
       <Pane weight={leftWeight}> 
         {left}
       </Pane>
